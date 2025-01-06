555f6829fa17184bc504f9b064fa8671b938c5de
Ganesh-Sharmaz <shashanklhr@gmail.com>
2025-01-06 21:14:44 +0530

    Added Vercel Analytics #2

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -8,7 +8,7 @@ function Layout() {
     
     return (
         <div className=" flex flex-col min-h-svh md:min-h-screen items-center w-full">
-            <Analytics/>
+            
             <Header />
             <main className=" flex flex-grow bg-mainBackground text-white w-full">
                 <Outlet />
@@ -16,6 +16,7 @@ function Layout() {
             <footer className="bg-mainBackground hidden md:block pt-3 md:pt-0 text-white md:px-10 py-3 w-full text-center">
                 <p>Everything that Yapper AI says is made up or Maybe not...</p>
             </footer>
+            <Analytics/>
         </div>
     );
 }
